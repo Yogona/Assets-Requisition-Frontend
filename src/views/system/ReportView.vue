6283cff90a0bee9702efc743739c7378bbdd1977
Yogona <yonagodwin@gmail.com>
2023-06-30 06:50:13 +0300

    report

--- src/views/system/ReportView.vue
+++ src/views/system/ReportView.vue
@@ -7,12 +7,19 @@ export default {
     },
     data() {
         return {
+            isLoading: false,
+            fromDate: null,
+            toDate: null,
+            uri: null,
             users: 0,
             departs: 0,
             stores: 0,
         };
     },
     methods: {
+        updateUri() {
+            this.uri = this.api + "/reports/generate/from/" + this.fromDate + "/to/" + this.toDate;
+        }
     },
     async mounted() {
     }
@@ -22,20 +29,28 @@ export default {
 <template>
     <main>
         <div class="h2 text-center">Report</div>
-        <div class="container card">
-            <div class="row justify-content-center mb-2">
-                <div class="col-sm-6">
-                    <label>From</label>
-                    <input type="date" class="form-control"  />
+        <div class="container card">{{ uri }}
+            <form method="GET" :action="uri" @submit.default="onSubmit">
+                <div class="row justify-content-center mb-2">
+                    <div class="col-sm-6">
+                        <label>From</label>
+                        <input type="date" @change="updateUri" v-model="fromDate" class="form-control" required  />
+                    </div>
+                    <div class="col">
+                        <label>To</label>
+                        <input type="date" @change="updateUri" v-model="toDate" class="form-control" required />
+                    </div>
                 </div>
-                <div class="col">
-                    <label>To</label>
-                    <input type="date" class="form-control" />
+                <div class="row mb-2">
+                    <button type="submit" class="btn btn-primary" :class="{disabled:isLoading}" >
+                        <span :hidden="isLoading">Generate</span>
+                        <div :hidden="!isLoading" class="spinner-border text-light" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </button>
                 </div>
-            </div>
-            <div class="row mb-2">
-                <button class="btn btn-primary">Generate</button>
-            </div>
+
+            </form>
         </div>
     </main>
 </template>
